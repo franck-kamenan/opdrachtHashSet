1826363eb258ed9a69b2eec156d8cdce2f5d9867
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-21 13:33:25 +0200

    Collection van Person

---
+++ PersonApp.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.opdrachten.opdracht2;
+
+import java.util.*;
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+
+        Collection<Person> personCol = new HashSet<>();
+
+        Person p1 = new Person("Jean-Marie", 73, 175);
+        Person p2 = new Person("Jean-Luc", 37, 185);
+        Person p3 = new Person("Jean-Luc", 37, 185);
+        Person p4 = new Person("Jean-Val-Jean", 53, 177);
+
+        personCol.add(p1);
+        personCol.add(p2);
+        personCol.add(p3);
+        personCol.add(p4);
+
+        personCol.forEach(System.out::println);
+    }
+}
