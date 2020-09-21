74d73a46f8782fc41786b1abc75b638ebe758eaa
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-21 11:44:48 +0200

    Random nextInt bound

--- LotteryApp.java
+++ LotteryApp.java
@@ -7,7 +7,9 @@ public class LotteryApp {
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
+        Random rng = new Random();
         Collection<Integer> userNumber = new HashSet<>();
+        Collection<Integer> winningNumbers = new HashSet<>();
 
         System.out.println("Give 6 unique numbers between 1 and 45.");
         while (userNumber.size() < 6) {
@@ -17,6 +19,15 @@ public class LotteryApp {
             }
         }
 
+        System.out.println();
         userNumber.forEach(System.out::println);
+
+        while (winningNumbers.size() < 6) {
+            int winningInput = rng.nextInt(45) + 1;
+            winningNumbers.add(winningInput);
+        }
+
+        System.out.println();
+        winningNumbers.forEach(System.out::println);
     }
 }
